87ed24412861c2259adda542e32dada3a1748fe1
Илья Смелов <kekkisama@gmail.com>
2022-02-23 22:13:55 +0300

    рабочая версия

--- Ski_resort/ClassSkiResort.py
+++ Ski_resort/ClassSkiResort.py
@@ -1,4 +1,5 @@
 import QuestTexts as Q
+import random
 
 
 class Day:
@@ -24,12 +25,116 @@ class Day:
     left_professional = 0  # Уехало Профессионалов
     arrived_professional = 0  # Приехало Профессионалов
 
-    income_newbie = newbie * 2000
+    income_newbie = 0
     income_professional = 0
     building = 0
     trail_maintenance = 10000
 
     count_building = 0
 
+    lift_occupancy = None
+    track_occupancy_professional = None
+    hotel_occupancy = None
+    track_occupancy_newbie = None
+    balance_for_tomorrow = None
 
-D = Day()
+    def get_arrived_left_people(self):
+        self.left_newbie = random.randint(3, 9)
+        self.arrived_newbie = random.randint(7, 13)
+        self.left_professional = random.randint(1, 3)
+        self.arrived_professional = random.randint(2, 6)
+
+        if self.popularity < 30:
+            self.left_newbie += 1
+            self.arrived_newbie -= 1
+            self.left_professional += 1
+            self.arrived_professional -= 1
+
+        elif self.popularity > 60:
+            self.left_newbie -= 1
+            self.arrived_newbie += random.randint(1, 3)
+            self.left_professional -= 1
+            self.arrived_professional += random.randint(1, 2)
+
+        if self.popularity > 70:
+            self.arrived_newbie += random.randint(1, 2)
+            self.arrived_professional += random.randint(1, 3)
+
+        if self.popularity > 85:
+            self.arrived_newbie += random.randint(1, 2)
+            self.arrived_professional += random.randint(2, 5)
+
+    def people(self):
+        count = 0
+        if self.hotel * Q.hotel[1] < self.newbie + self.professional:
+            count += (self.newbie + self.professional) - self.hotel * Q.hotel[1]
+
+        if self.restauran * Q.restauran[1] < self.newbie + self.professional:
+            count += ((self.newbie + self.professional) - self.restauran * Q.restauran[1])
+
+        if (self.lift * Q.lift[1] + self.kupe_lift * Q.kupe_lift[1]) < self.newbie + self.professional:
+            count += ((self.newbie + self.professional) - (self.lift * Q.lift[1]
+                                                           + self.kupe_lift * Q.kupe_lift[1]))
+
+        if (self.low_track * Q.low_track[2] + self.middle_track * Q.middle_track[2]) < self.newbie:
+            count_newbie = (self.newbie - (self.low_track * Q.low_track[2]
+                                           + self.middle_track * Q.middle_track[2]))
+            self.popularity -= count_newbie
+            self.newbie -= count_newbie
+            self.left_newbie += count_newbie
+
+        if (self.high_track * Q.high_track[2] + self.middle_track * Q.middle_track[2]) < self.professional:
+            count_newbie = (self.professional - (self.high_track * Q.high_track[2]
+                                                 + self.middle_track * Q.middle_track[2]))
+            self.popularity -= count_newbie * 2
+            self.professional -= count_newbie
+            self.left_professional += count_newbie
+
+        self.newbie -= int(count / 2)
+        self.professional -= int(count / 2)
+        self.popularity -= int(count * 1.5)
+        self.left_newbie += int(count / 2)
+        self.left_professional += int(count / 2)
+
+    def profit_newbie(self):
+        self.income_newbie = self.newbie * 2000
+
+    def profit_professional(self):
+        self.income_professional = self.professional * 6000
+
+    def get_trail_maintenance(self):
+        self.trail_maintenance = (self.low_track * Q.low_track[1]
+                                  + self.middle_track * Q.middle_track[1]
+                                  + self.high_track * Q.high_track[1])
+
+    def get_arrived_left_newbie(self):
+        self.newbie += self.arrived_newbie - self.left_newbie
+
+    def get_arrived_left_professional(self):
+        self.professional += self.arrived_professional - self.left_professional
+
+    def get_lift_occupancy(self):
+        self.lift_occupancy = self.kupe_lift * Q.kupe_lift[1] + self.lift * Q.lift[1]
+
+    def get_hotel_occupancy(self):
+        self.hotel_occupancy = int((self.newbie + self.professional)
+                                   * 100 / (self.hotel * Q.hotel[1]))
+
+    def get_track_occupancy_newbie(self):
+        self.track_occupancy_newbie = (self.low_track * Q.low_track[2]
+                                       + self.middle_track * Q.middle_track[2])
+
+    def get_track_occupancy_professional(self):
+        self.track_occupancy_professional = (self.high_track * Q.high_track[2]
+                                             + self.middle_track * Q.middle_track[2])
+
+    def get_balance_for_tomorrow(self):
+        self.profit_newbie()
+        self.profit_professional()
+        self.balance_for_tomorrow = (self.cr + self.income_newbie
+                                     + self.income_professional
+                                     - self.building
+                                     - self.trail_maintenance
+                                     - self.advertising)
+
+d = Day()
--- Ski_resort/SkiResort.py
+++ Ski_resort/SkiResort.py
@@ -1,215 +1,275 @@
 import random
 import QuestTexts as Q
-from ClassSkiResort import D
+from ClassSkiResort import Day
 
-def new_day():
-    D.day += 1
-    
-    D.left_newbie = random.randint(3, 9)
-    D.arrived_newbie = random.randint(7, 13)
-    D.left_professional = random.randint(1, 3)
-    D.arrived_professional = random.randint(2, 5)
 
-    if D.day == 1:
-        D.left_professional = 0
+def new_day(d):
+    d.day += 1
+    d.building = 0
+    d.count_building = 0
 
-    if D.popularity < 30:
-        D.left_newbie += random.randint(1, 2)
-        D.arrived_newbie -= 1
-        D.left_professional += 1
-        D.arrived_professional -= 1
-    elif D.popularity > 60:
-        D.left_newbie -= 1
-        D.arrived_newbie += random.randint(1, 3)
-        D.left_professional -= 1
-        D.arrived_professional += random.randint(1, 2)
+    d.profit_newbie()
+    d.profit_professional()
+    d.cr += d.income_newbie + d.income_professional
 
-    D.income_newbie = D.newbie * 2000
-    D.income_professional = D.professional * 6000
-    D.old_professional = D.professional
-    D.old_newbie = D.newbie
+    d.get_arrived_left_people()
 
-    D.trail_maintenance = (D.low_track * Q.low_track[1]
-                         + D.middle_track * Q.middle_track[1]
-                         + D.high_track * Q.high_track[1])
+    if d.day == 1:
+        d.left_professional = 0
+    d.people()
+    d.old_professional = d.professional
+    d.old_newbie = d.newbie
 
-    D.cr += D.income_newbie + D.income_professional
+    d.get_arrived_left_newbie()  # новичков приехало и уехало
+    d.get_arrived_left_professional()
 
-    D.newbie += D.arrived_newbie - D.left_newbie  # новичков приехало и уехало
-    D.professional += D.arrived_professional - D.left_professional
+    d.get_trail_maintenance()
+    d.cr -= d.trail_maintenance
+    d.cr -= d.advertising
 
-    D.cr -= D.trail_maintenance
-    D.cr -= D.advertising
+    d.popularity += int((d.advertising / 10000) * random.randint(3, 5))
+    d.popularity -= random.randint(5, 10)
 
-    D.popularity += int((D.advertising / 10000) * random.randint(3, 5))
-    D.popularity -= random.randint(5, 10)
-    if D.popularity < 0 or D.cr < 0:
-        print("Вы проиграли")
+    if d.popularity < 0:
+        print(Q.the_end[0])
+        return main(START)
+    if d.day > 20:
+        print(Q.the_end[0])
+        return main(START)
+    if d.cr < 0:
+        print(Q.the_end[1].format(name=name))
+        return main(START)
+    if d.cr > 1000000 and d.day <= 20 and START is False:
+        if d.popularity > 60:
+            d.popularity -= 40
+        elif 60 > d.popularity > 40:
+            d.popularity -= 20
+        print(Q.end_or[0].format(name=name))
+        return main(d)
+    if d.cr > 10000000:
+        print(Q.end_or[1].format(name=name))
+        return main(START)
+
     print("================================================================")
     n_day = random.randint(0, 2)
     if n_day == 2:
-        D.cr -= 1000
+        d.cr -= 1000
+    elif n_day == 3:
+        d.popularity += random.randint(2, 4)
     print(Q.qwerty_break[n_day])
-    return the_game()
+    return the_game(d)
 
 
-def get_advertising():
-    print(*Q.qwerty_advertising, sep="\n")
+def get_advertising(d):
     choice = 0
+    num_choice = int(d.advertising / 10000)
     while choice not in (6, 7):
-        choice = int(input())
-        if choice in (1, 2, 3, 4, 5):
-            D.advertising = int(choice * 10000)
-            print(f"Окай мы будем тратить на рекламу {D.advertising}. "
-                  f"Что нибудь еще?")
+        count = 0
+        if d.advertising == 0:
+            for i in Q.qwerty_advertising:
+                if count != 0:
+                    print(i)
+                count += 1
+        else:
+            for i in Q.qwerty_advertising:
+                if count != num_choice:
+                    print(i)
+                count += 1
+
+        choice = input()
+        if choice.isdigit():
+            choice = int(choice)
+
+        if d.advertising != 0:
+            print()
+        if choice in (0, 1, 2, 3, 4, 5) and choice != num_choice:
+            num_choice = choice
+            d.advertising = choice * 10000
+            print(f"\nБудет сделано сказал менеджер по рекламе. "
+                  f"- Текущие расходы на рекламу равны {d.advertising} cr в день. ")
+            print()
         elif choice == 6:
-            return talk()
+            return talk(d)
         elif choice == 7:
-            return new_day()
+            return new_day(d)
+        num_choice = choice
 
 
-def talk():
-    lift_occupancy = D.kupe_lift * Q.kupe_lift[1] + D.lift * Q.lift[1]
-    hotel_occupancy = (((D.hotel * Q.hotel[1]) / 100)
-                       * (D.newbie + D.professional))
-    track_occupancy_newbie = (D.low_track * Q.low_track[2]
-                              + D.middle_track * Q.middle_track[2])
-    track_occupancy_professional = (D.high_track * Q.high_track[2]
-                                    + D.middle_track * Q.middle_track[2])
-    balance_for_tomorrow = (D.cr + D.income_newbie
-                            + D.income_professional
-                            - D.building
-                            - D.trail_maintenance
-                            - D.advertising)
-    advisors = {'cr': D.cr,
-                'income': D.income_newbie + D.income_professional,
-                'maintenance': D.trail_maintenance,
-                'advertising': D.advertising,
-                'building': D.building,
-                'balance': balance_for_tomorrow,
-                'people': D.newbie + D.professional,
-                'newbie': D.newbie,
-                'professional': D.professional,
-                'hotel': hotel_occupancy,
-                'restauran': D.restauran * Q.restauran[1],
-                'lift': lift_occupancy,
-                'track_newbie': track_occupancy_newbie,
-                'track_professional': track_occupancy_professional,
-                'popularity': D.popularity}
+def talk(d):
+    d.profit_newbie()
+    d.profit_professional()
+    d.get_lift_occupancy()
+    d.get_hotel_occupancy()
+    d.get_track_occupancy_newbie()
+    d.get_track_occupancy_professional()
+    d.get_balance_for_tomorrow()
+
+    advisors = {'cr': d.cr,
+                'income': d.income_newbie + d.income_professional,
+                'maintenance': d.trail_maintenance,
+                'advertising': d.advertising,
+                'building': d.building,
+                'balance': d.balance_for_tomorrow,
+                'people': d.newbie + d.professional,
+                'newbie': d.newbie,
+                'professional': d.professional,
+                'hotel': d.hotel_occupancy,
+                'restauran': d.restauran * Q.restauran[1],
+                'lift': d.lift_occupancy,
+                'track_newbie': d.track_occupancy_newbie,
+                'track_professional': d.track_occupancy_professional,
+                'popularity': d.popularity}
 
     your_choice = 0
     while your_choice not in (4, 5, 6):
         print(Q.qwerty_2[0], *Q.qwerty_3, sep="\n")
-        if D.day != 0:
+        if d.day != 0:
             print(Q.qwerty_3_1)
-        your_choice = int(input()) - 1
+
+        your_choice = input()
+        if your_choice.isdigit():
+            your_choice = int(your_choice) - 1
+
         if your_choice in [0, 1, 2, 3, 4]:
             print(Q.talk[your_choice].format(**advisors))
+            print()
             if your_choice == 4:
-                return get_advertising()
+                return get_advertising(d)
         elif your_choice == 5:
-            return new_day()
-        elif D.day != 0 and your_choice == 6:
-            return menu()
+            return new_day(d)
+        elif d.day != 0 and your_choice == 6:
+            return menu(d)
 
 
-def get_building():
+def get_building(d):
     choice_building = 0
     while choice_building != 7:
         print(Q.qwerty_2[1])
-        choice_building = (int(input(Q.qwerty_building)) - 1)
+
+        choice_building = input(Q.qwerty_building)
+        if choice_building.isdigit():
+            choice_building = int(choice_building) - 1
+
         if choice_building in (0, 1, 2, 3, 4, 5, 6):
-            count = D.cr - Q.company_list[choice_building][0]
-            if D.count_building <= 2 and count > 0:
-                D.cr -= Q.company_list[choice_building][0]
+            count = d.cr - Q.company_list[choice_building][0]
+            if d.count_building < 2 and count > 0:
+                d.cr -= Q.company_list[choice_building][0]
                 if choice_building == 0:
-                    D.hotel += 1
+                    d.hotel += 1
                 elif choice_building == 1:
-                    D.restauran += 1
+                    d.restauran += 1
                 elif choice_building == 2:
-                    D.low_track += 1
+                    d.low_track += 1
                 elif choice_building == 3:
-                    D.middle_track += 1
+                    d.middle_track += 1
                 elif choice_building == 4:
-                    D.high_track += 1
+                    d.high_track += 1
                 elif choice_building == 5:
-                    D.lift += 1
+                    d.lift += 1
                 elif choice_building == 6:
-                    D.kupe_lift += 1
-                D.count_building += 1
-            else:
-                print("Недостаточно средств")
+                    d.kupe_lift += 1
+                d.building += Q.company_list[choice_building][0]
+                d.count_building += 1
+                d.popularity += random.randint(3, 5)
+                print(Q.qwerty_4[choice_building])
+            elif d.count_building == 2:
+                print("\nВы не можете строить больше двух сооружений в день")
+                return menu(d)
+            elif count < 0:
+                print("\nНедостаточно средств")
         elif choice_building == 7:
-            return menu()
+            return menu(d)
 
 
-def one_day():
+def one_day(d):
     print(Q.start_text, *Q.qwerty_one, sep="\n")
     want_to_play = 0
     while want_to_play != 1 and want_to_play != 2:
-        want_to_play = int(input())
+
+        want_to_play = input()
+        if want_to_play.isdigit():
+            want_to_play = int(want_to_play)
+
         if want_to_play == 1:
-            return talk()
+            return talk(d)
         elif want_to_play == 2:
-            return new_day()
+            return new_day(d)
 
 
-def menu():
+def menu(d):
     print(*Q.qwerty, sep='\n')
     play = 0
     while play != 1 and play != 2 and play != 3:
-        play = int(input())
+
+        play = input()
+        if play.isdigit():
+            play = int(play)
+
         if play == 1:
-            return talk()
+            return talk(d)
         elif play == 2:
-            return get_building()
+            return get_building(d)
         elif play == 3:
-            return new_day()
+            return new_day(d)
         play = 0
 
 
-def the_game():
-    lift_places = (D.lift * Q.lift[1]) + (D.kupe_lift * Q.kupe_lift[1])
-    hotel_places = D.hotel * Q.hotel[1]
-    restauran_places = D.restauran * Q.restauran[1]
-    track_occupancy_newbie = (D.low_track * Q.low_track[2]
-                              + D.middle_track * Q.middle_track[2])
-    track_occupancy_professional = (D.high_track * Q.high_track[2]
-                                    + D.middle_track * Q.middle_track[2])
-    days_list = {"day": D.day,
-                 "left_days": 20 - D.day,
-                 "it_was_newbie": D.old_newbie,
-                 "newbie_left": D.left_newbie,
-                 "newbie_arrived": D.arrived_newbie,
-                 "now_newbie": D.newbie,
-                 "it_was_professional": D.old_professional,
-                 "professional_left": D.left_professional,
-                 "professional_arrived": D.arrived_professional,
-                 "now_professional": D.professional,
-                 "cr_newbie": D.income_newbie,
-                 "cr_professional": D.income_professional,
-                 "cr_track": D.trail_maintenance,
-                 "cr_advertising": D.advertising,
-                 "cr": D.cr,
-                 "popularity": D.popularity,
-                 "hotel": D.hotel,
+def the_game(d):
+    lift_places = (d.lift * Q.lift[1]) + (d.kupe_lift * Q.kupe_lift[1])
+    hotel_places = d.hotel * Q.hotel[1]
+    restauran_places = d.restauran * Q.restauran[1]
+    track_occupancy_newbie = (d.low_track * Q.low_track[2]
+                              + d.middle_track * Q.middle_track[2])
+    track_occupancy_professional = (d.high_track * Q.high_track[2]
+                                    + d.middle_track * Q.middle_track[2])
+    days_list = {"day": d.day,
+                 "left_days": 20 - d.day,
+                 "it_was_newbie": d.old_newbie,
+                 "newbie_left": d.left_newbie,
+                 "newbie_arrived": d.arrived_newbie,
+                 "now_newbie": d.newbie,
+                 "it_was_professional": d.old_professional,
+                 "professional_left": d.left_professional,
+                 "professional_arrived": d.arrived_professional,
+                 "now_professional": d.professional,
+                 "cr_newbie": d.income_newbie,
+                 "cr_professional": d.income_professional,
+                 "cr_track": d.trail_maintenance,
+                 "cr_advertising": d.advertising,
+                 "cr": d.cr,
+                 "popularity": d.popularity,
+                 "total_people": d.newbie + d.professional,
+                 "hotel": d.hotel,
                  "hotel_places": hotel_places,
-                 "restauran": D.restauran,
+                 "restauran": d.restauran,
                  "restauran_places": restauran_places,
                  "lift_places": lift_places,
                  "track_newbie": track_occupancy_newbie,
                  "track_professional": track_occupancy_professional}
-
     print(Q.days.format(**days_list))
-    return menu()
+    return menu(d)
 
 
-while True:
-    want_to_play = input(Q.beginning_of_the_game)
-    if want_to_play.lower() in ["начать новую игру", "начать", "1"]:
-        one_day()
-    elif want_to_play.lower() in ["продолжить", "2"]:
-        the_game()
-    elif want_to_play.lower() in ["закончить", "завершить", "закрыть", "3"]:
-        break
+def main(d):
+    if d is False:
+        d = Day()
+        print(Q.beginning_of_the_game[0],
+              Q.beginning_of_the_game[2])
+    else:
+        print(*Q.beginning_of_the_game)
+    global START
+    while True:
+        want_to_play = input()
+        if want_to_play.lower() in ["начать новую игру", "начать", "1"]:
+            START = False
+            one_day(d)
+        elif want_to_play.lower() in ["продолжить", "2"] and d == 0:
+            START = True
+            the_game(d)
+        elif want_to_play.lower() in ["закончить", "завершить", "закрыть", "3"]:
+            break
 
+
+name = 'Пивной Фей'
+START = False
+main(START)
